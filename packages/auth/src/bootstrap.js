d6ca43d0a40d8c3c10f2ee44b5a96bb65a9f4736
Yusuf Ziya Korol <yusufziyakorol@Yusufs-MacBook-Air.local>
2022-05-16 20:48:08 +0300

    setup auth project

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (!!onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathName }) => {
+      console.log(nextPathName);
+      const { pathName } = history.location;
+
+      if (pathName !== nextPathName) {
+        history.push(nextPathName);
+      }
+    },
+  };
+};
+
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#_auth-dev-root");
+  if (el) {
+    mount(el, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+export { mount };
