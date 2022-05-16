15f2067b0c56f63c1d0bab9c67aa1bc6adf10882
Yusuf Ziya Korol <yusufziyakorol@Yusufs-MacBook-Air.local>
2022-05-16 23:55:06 +0300

    add progress component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
+import Progress from "./components/Progress";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -19,7 +20,7 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Progress />}>
             <Switch>
               <Route path="/auth" component={AuthLazy} />
               <Route path="/" component={MarketingLazy} />
